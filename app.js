35c17014bc56ff50a7f40adc2c0287ec1f93759b
Pauline Justine Capacia <104846864+paulinejustine@users.noreply.github.com>
2023-10-11 21:17:06 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+ 
+//synchronous
+
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+ 
+// asynchronous
+fs.readdir('./', function(err, files){
+    if (err) console.log('Error', err);
+    else console.log('Result', files)
+});
---
+++ logger.js
@@ -0,0 +1,10 @@
+console.log(__filename);
+console.log(__dirname);
+
+url = 'https://mylogger.io/log';
+
+function log(message){
+    console.log(message);
+}
+
+module.exports = log;
